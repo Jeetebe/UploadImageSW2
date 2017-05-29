0d0c87131e127b529ec5b1fa1ce0d03c830d7ede
cuscedu <cuscedu@mac01.local>
2017-05-29 16:19:38 +0700

    Initial Commit

---
+++ AlamoUpload/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  AlamoUpload
+//
+//  Created by Adi Nugroho on 5/18/16.
+//  Copyright © 2016 Adi Nugroho. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imgView: UIImageView!
+    
+    let picker = UIImagePickerController()
+    var pickedImagePath: NSURL?
+    var pickedImageData: NSData?
+    
+    var localPath: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        picker.allowsEditing = false
+        picker.delegate = self
+    }
+    
+    @IBAction func onBtnOpenClicked(sender: UIButton) {
+        presentViewController(picker, animated: true, completion: nil)
+    }
+
+    @IBAction func onBtnSubmitClicked(sender: UIButton) {
+        guard let path = localPath else {
+            return
+        }
+        
+        Alamofire.upload(.POST, "http://172.18.200.201:8080/FileUploaderRESTService/rest/upload"
+            , multipartFormData: { formData in
+                let filePath = NSURL(fileURLWithPath: path)
+                formData.appendBodyPart(fileURL: filePath, name: "file")
+                formData.appendBodyPart(data: "Alamofire".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!, name: "file")
+            }, encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .Success:
+                    print("SUCCESS")
+                case .Failure(let error):
+                    print(error)
+                }
+        })
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            return
+        }
+        
+        imgView.image = image
+        
+        let documentDirectory: NSString = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first!
+        
+        let imageName = "temp"
+        let imagePath = documentDirectory.stringByAppendingPathComponent(imageName)
+        
+        if let data = UIImageJPEGRepresentation(image, 80) {
+            data.writeToFile(imagePath, atomically: true)
+        }
+        
+        localPath = imagePath
+        
+        dismissViewControllerAnimated(true, completion: {
+            
+        })
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
